5af0db0c250385ed4f3d6721f45ed47d8f789243
moglieswarachary-droid <moglieswarachary@gmail.com>
2026-09-18 10:02:56 +0530

    chore(arch): add barrel exports for components/shared and enforce ARCHITECTURE.md compliance

---
+++ src/components/shared/index.ts
@@ -0,0 +1,5 @@
+export * from './CommandPalette'
+export * from './BiometricPromptModal'
+export * from './CredentialScannerModal'
+export * from './PermissionGate'
+export * from './SessionLockOverlay'
